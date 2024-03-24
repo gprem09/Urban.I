4ae27323287d052f2ab05edfb7b8e7d581373f8c
Patticatti <xpatticattix@gmail.com>
2024-03-24 05:35:27 -0700

    added buttons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,25 @@
-import { Flex, Heading, Button, Input } from "@chakra-ui/react";
+import { Flex, Heading, Button, Stack } from "@chakra-ui/react";
 export const Footer = () => {
   return (
-    <Flex width="100%" height="300px" bg="lgray">
+    <Stack
+      width="100%"
+      height="300px"
+      bg="lgray"
+      alignItems="center"
+      padding="2rem"
+    >
       <Heading fontSize="2xl" maxWidth="500px">
         Sign up to our newsletter and receive the latest updates on your
         favourite projects.
       </Heading>
-      <Input
-        variant="outline"
-        color="black"
-        border="black"
-        placeholder="Email Address..."
-      ></Input>
-      <Button variant="white">Email Address</Button>
-    </Flex>
+      <Button
+        variant="white"
+        width="300px"
+        justifyContent="flex-start"
+        opacity="0.6"
+      >
+        Email Address
+      </Button>
+    </Stack>
   );
 };
