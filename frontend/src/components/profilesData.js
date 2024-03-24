0d457b02359105c6870f62ae7875261287ba65cb
gpa21 <gpa21@sfu.ca>
2024-03-23 20:05:02 -0700

    update

--- frontend/src/components/profilesData.js
+++ frontend/src/components/profilesData.js
@@ -28,6 +28,30 @@ const profiles = [
         country: 'United Kingdom',
         profile: 'We are engineers with a passion and expertise in the design and delivery of high quality award winning buildings, structures and public spaces. We who care about people and the environments they occupy. We provide expertise to help developers and architects bring their plans to life – whether it is converting historic buildings, designing new buildings or creating new public spaces. We also work with artists to help realise their creative ambitions often complex installations.',
         website: 'https://civicengineers.com/'
+    },
+    {
+        name: 'Studio Egret West',
+        country: 'United Kingdom',
+        profile: 'Studio Egret West was established in November 2004 by Christophe Egret and David West with a shared vision: To offer strategy with architectural specificity; and specificity within an overall strategy. Studio Egret West brings surprise and delight to place making. Architecture is too often seen in isolation from its urban context. Planning is too often seen as soulless, unspecific proposals that gather dust on shelves. We believe that the way forward is the osmosis between planning and architecture. Studio Egret West is just this.',
+        website: 'https://egretwest.com/'
+    },
+    {
+        name: 'Nick Moss Architects',
+        country: 'United Kingdom',
+        profile: 'Nick Moss Architects (previously known as Sixtwo Architects), is a Manchester architectural and urban design practice with a national reputation for design quality and client focused service. Set up in 2012, the practice has received many awards in this time, including the prestigious RIBA Emerging Practice award. With a progressive and responsive architectural approach, excellence comes as standard as they continue their passion for making brilliant homes and workspaces.',
+        website: 'https://nickmossarchitects.com/'
+    },
+    {
+        name: 'stephenson STUDIO',
+        country: 'United Kingdom',
+        profile: 'Established in 1979 by Roger Stephenson OBE, Stephenson Studio is an architectural and urban design practice born in Manchester. They have a long track record of creating inspiring buildings and spaces, and have spent the last 38 years designing the urban renaissance of the North West. Urban Splash first collaborated with Stephenson Studio (then Stephenson Bell) 25 years ago, with Smithfield Building, just around the corner in Manchesters hip Northern Quarter. Their philosophy and architectural ethos has resulted in the practice winning more than 150 national awards for architecture, and they have extensive experience of working with buildings of historical significance, like Avro. Smithfield Building served as the catalyst for the regeneration of this area, and helped to inspire a loft living revolution across the North West of England. Since that time, Stephenson Studio have completed many of Manchesters most significant buildings, including Hallé, St. Peters in Ancoats, the Free Trade Hall Hotel and Chethams School of Music, which was short-listed for the Stirling Prize.',
+        website: 'http://www.stephenson-studio.com/'
+    },
+    {
+        name: 'Hawkins\Brown',
+        country: 'United Kingdom',
+        profile: 'Hawkins\Brown asks questions, listens carefully and then thinks hard before putting pen to paper. Our broad experience of building has given us the skills, knowledge and drive to design and manage complex schemes like Park Hill in Sheffield, Corby Civic Hub and the design and construction of the New Biochemistry building for Oxford University. We are Hawkins\Brown: architects\ designers\ masterplanners\ researchers\ space planners\ interior designers\ collaborators\ makers\ negotiators\ artists\ visualisers\ problem solvers...',
+        website: 'https://www.hawkinsbrown.com/'
     }
 ];
 
